180a95e158b047e9e03c74b60b101b814c4dc58f
Mickey <hedgefund996@gmail.com>
2022-09-13 22:41:07 +0800

    get deposit & info -> return new validator

--- beacon-chain/get_validator_from_deposit.py
+++ beacon-chain/get_validator_from_deposit.py
@@ -1,6 +1,11 @@
-# beacon chain spec -> validator deposit contract (EIP-2982)
+# 
+# ! beacon chain spec -> validator deposit contract (EIP-2982)
+# * 这里主要就是beacon chain接收信息（BeaconState + Deposit），返回新validator的处理逻辑
 def get_validator_from_deposit(state: BeaconState, deposit: Deposit) -> Validator:
+    # * amount就是用户传进来的质押金额 -> 最少32ETH（定义成了一个constant）
     amount = deposit.data.amount
+    # ! EFFECTIVE_BALANCE_INCREMENT = Gwei(2**0 * 10**9) (= 1,000,000,000)
+    # ! MAX_EFFECTIVE_BALANCE = Gwei(2**5 * 10**9) (= 32,000,000,000)
     effective_balance = min(amount - amount % EFFECTIVE_BALANCE_INCREMENT, MAX_EFFECTIVE_BALANCE)
 
     return Validator(
